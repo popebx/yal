c625812253578c3891a6b38f044e213aa860e0c7
pushecx <philipp.hetfeld@tu-dortmund.de>
2018-11-22 19:50:37 +0100

    Example cleanup

--- example/yal_singleton.cpp
+++ example/yal_singleton.cpp
@@ -4,8 +4,8 @@ int main() {
   using namespace std::string_view_literals;
   yalog::make_logger("cout", yalog::sinks::console_out{},
                      yalog::log_level::WARNING);
-  ylog.set_as_default_logger("cout");
-  ylog("cout");
+  ylog.set_default_logger("cout");
+  ylog("cout").debug().print("%x", 5);
   ylog().warn() << "Hello World\n"sv;
   return 0;
 }
